127e90c21ac8d7f5f3b566b8634def26c6ffc221
Angelo <castromatheus060@gmail.com>
2021-09-04 15:13:59 -0300

    feat: send and receive

---
+++ hash.py
@@ -0,0 +1,38 @@
+from cryptography.fernet import Fernet
+
+def send(message): 
+    key = Fernet.generate_key()
+    with open('secret.key', 'wb') as keyFile:
+        keyFile.write(key)
+    cipherSuite = Fernet(key)
+    cipherText = cipherSuite.encrypt(message)
+    print('chiper message: ' + cipherText.decode('utf-8') + ' key: ' + key.decode('utf-8'))
+
+def loadKey():  
+    return open('secret.key', 'rb').read()
+    
+def receive(message, isKey):
+    try: 
+        if(isKey): Key = bytes(isKey, encoding= 'utf-8')
+        else: Key = loadKey()
+
+        cipherSuite = Fernet(Key)    
+        plainText = cipherSuite.decrypt(message)
+
+        print(plainText.decode('utf-8'))                
+    except:
+        print('Null')
+
+while True:
+    directionQuestion = input('Press 1 to send Message, 2 to receive message and 3 to exit: ')
+    if directionQuestion == '1':
+        message = input('Input your text ')
+        send(bytes(message, encoding= 'utf-8'))
+
+    if directionQuestion == '2':
+        chiperMessage = input('Input your chiper message: ')
+        key = input('Input your key: ')
+        receive(bytes(chiperMessage, encoding= 'utf-8'), key)
+        
+    if directionQuestion == '3':
+        break
